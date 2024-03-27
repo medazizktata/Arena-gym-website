27cd5826f141524f803e0819eabd7dd4fdde115e
medazizktata <aziiizktata7@gmail.com>
2024-03-27 02:45:13 +0100

    Ajout Bouton Section Activités

--- Arena-gym-website/src/pages/Home/ActivitiesSection.jsx
+++ Arena-gym-website/src/pages/Home/ActivitiesSection.jsx
@@ -1,19 +1,29 @@
 import BackgroundText from "../../components/BackgroundText";
+import ButtonYellow from "../../components/Buttons/ButtonYellow";
 
 function ActivitiesSection() {
   return (
     <>
-      <div className=" my-10 relative">
-        <h2 className="font-urbanist text-white text-3xl relative z-10">
+      <div className="my-20 relative">
+        <h2 className="font-urbanist ml-[10%] font-bold text-white text-3xl relative z-10">
           Découvrez nos activités
         </h2>
         <BackgroundText
           className=""
-          text={"Activités"}
+          text={"Activites"}
           fontFamily={"urbanist"}
           strokeColor={"white"}
           textSize={20}
         />
+      </div>
+      <div className="flex flex-col justify-center items-center text-lg">
+        <ButtonYellow
+          buttonFont="font-robotoCon"
+          fontWeight="font-black"
+          paddingX={10}
+        >
+          Voir nos Activités
+        </ButtonYellow>
       </div>
     </>
   );
