dc8d205a2d3b15a0cc98f9f042f6743540c170c5
medazizktata <aziiizktata7@gmail.com>
2024-03-27 02:45:52 +0100

    Fixed BackgroundText Component extra bottom space leftover

--- Arena-gym-website/src/components/BackgroundText.jsx
+++ Arena-gym-website/src/components/BackgroundText.jsx
@@ -1,13 +1,31 @@
-import { getColor } from "../utils/getColor"; // assuming you have a utility function to translate color names to hex codes
+import { useEffect, useRef, useState } from "react";
+import { getColor } from "../utils/getColor";
 
 const BackgroundText = ({ text, fontFamily, strokeColor, textSize }) => {
+  const [svgWidth, setSvgWidth] = useState(0);
+  const [svgHeight, setSvgHeight] = useState(0);
+  const svgRef = useRef(null);
+
+  useEffect(() => {
+    if (svgRef.current) {
+      const bbox = svgRef.current.getBBox();
+      setSvgWidth(bbox.width);
+      setSvgHeight(bbox.height);
+    }
+  }, [text, fontFamily, strokeColor, textSize]);
+
   const translatedColor = getColor(strokeColor);
+
   return (
     <div className="w-full text-center">
-      <svg viewBox="0 0 100 100" className="w-full">
+      <svg
+        viewBox={`0 0 ${svgWidth} ${svgHeight}`}
+        className="w-full"
+        ref={svgRef}
+      >
         <text
-          x="50%"
-          y="15%"
+          x={svgWidth / 2}
+          y={svgHeight / 2}
           dominantBaseline="middle"
           textAnchor="middle"
           fill="none"
