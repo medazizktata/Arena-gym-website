1c364bb3783dfa21cdaa442af08ca87c7129ef0a
medazizktata <aziiizktata7@gmail.com>
2024-03-27 02:46:15 +0100

    Created Coaches Section

--- Arena-gym-website/src/pages/Home/Home.jsx
+++ Arena-gym-website/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import HeroSection from "./HeroSection";
 import ActivitiesSection from "./ActivitiesSection";
+import CoachesSection from "./CoachesSection";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Navbar />
       <HeroSection />
       <ActivitiesSection/>
+      <CoachesSection/>
     </>
   );
 }
