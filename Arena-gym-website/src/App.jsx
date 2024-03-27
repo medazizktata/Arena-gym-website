c539cd1578f0435310500a23fb00b69b21936726
Bedis <bedis.baccar@etudiant-enit.utm.tn>
2024-03-27 17:31:48 +0100

    map

--- Arena-gym-website/src/App.jsx
+++ Arena-gym-website/src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import FontTesting from './fontTesting.jsx';
 import SplitRectangle from './Sokra_only.jsx';
-
+import MapCard from './map.jsx';
 function App() {
   return (
 
     <div >
       
-      <FontTesting/>
-
-
-      <SplitRectangle
+           <SplitRectangle
       text_grand="Fitness d'Élite Padel & Piscines Premium"
       text="Plongez dans une expérience fitness hors du commun avec nos terrains de padel et piscines exclusives, intégrés à notre offre haut de gamme. Rejoignez-nous pour une remise en forme qui conjugue luxe, confort et performance."
       imageUrl="https://www.piscineavenue.com/wp-content/uploads/2020/04/prix-construction-piscine-olympique-600x321.jpeg"
       />
-    
+
+      <MapCard
+      title="Visitez-nous à Manar 2"
+      subtitle="Venez nous rendre visite pour découvrir ce que nous offrons pour améliorer votre condition physique !"
+      url="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12772.765663728429!2d10.1565846!3d36.8378915!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd3368ff1aaaad%3A0x6971f9e8f53b0353!2sArena%20gym!5e0!3m2!1sfr!2stn!4v1711553784567!5m2!1sfr!2stn"
+      />
    </div>
   );
 }
--- Arena-gym-website/src/Sokra_only.jsx
+++ Arena-gym-website/src/Sokra_only.jsx
@@ -2,14 +2,14 @@
 
 
 // SplitRectangle.jsx
-const SplitRectangle = ({ text_grand, text, imageUrl }) => {
+/*const SplitRectangle = ({ text_grand, text, imageUrl }) => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="outer-box flex overflow-hidden max-w-4xl mx-auto">
         <div className="trap-left w-1/2">
           <div className="inner-left-border"></div>
-          <h1 className="text-3xl font-bold mb-4">{text_grand}</h1> {/* Ajustez ces classes selon la taille souhaitée */}
-          <p>{text}</p> {/* Ajoutez les classes pour le texte ici si nécessaire */}
+          <h1 className="text-3xl font-bold mb-4">{text_grand}</h1> 
+          <p>{text}</p> 
         </div>
         <div className="trap-right w-1/2">
           <div className="inner-right-border"></div>
@@ -20,4 +20,31 @@ const SplitRectangle = ({ text_grand, text, imageUrl }) => {
   );
   };
   
+  */
+
+  const SplitRectangle = ({ text_grand, text, imageUrl }) => {
+    return (
+      <div className="mx-auto max-w-4xl px-4 py-9">
+        <div className="flex">
+          <div className="text-part flex-1 p-4" 
+               style={{
+                 clipPath: 'polygon(0 0, 100% 0, 90% 100%, 0% 100%)',
+                 border: '4px solid #FBDE06'
+               }}>
+            <h2 className="text-xl font-bold">{text_grand}</h2>
+            <p>{text}</p>
+          </div>
+          <div className="image-part flex-1" 
+               style={{
+                 clipPath: 'polygon(10% 0, 100% 0, 100% 100%, 0 100%)',
+                 border: '4px solid #FBDE06'
+               }}>
+            <img src={imageUrl} alt="Split Rectangle Image" className="w-full h-auto" />
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+
   export default SplitRectangle
