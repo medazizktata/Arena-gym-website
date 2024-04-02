e72b9d07d9893a654926697e8d07437f94bb49da
medazizktata <aziiizktata7@gmail.com>
2024-04-02 02:43:33 +0200

    Home comp fix

--- Arena-gym-website/src/pages/Home/Home.jsx
+++ Arena-gym-website/src/pages/Home/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import Navbar from '../../components/Navbar';
-
+import React from "react";
+import Navbar from "../../components/Navbar";
+import HeroSection from "./HeroSection";
+import ActivitiesSection from "./ActivitiesSection";
+import CoachesSection from "./CoachesSection";
+import About from "../About/About";
 
 function Home() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      {/* <Navbar />
+      <HeroSection />
+      <ActivitiesSection/>
+      <CoachesSection/> */}
+      <About/>
+    </>
   );
 }
 
