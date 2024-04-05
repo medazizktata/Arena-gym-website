c04a593b19852ba38842bef2573876998d968aef
medazizktata <aziiizktata7@gmail.com>
2024-04-05 05:00:15 +0200

    Path fix

--- Arena-gym-website/src/App.jsx
+++ Arena-gym-website/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './pages/Home/Home.jsx';
-import Inscription from './pages/inscription/inscription.jsx';
+import Inscription from './pages/Inscription/inscription.jsx';
 
 function App() {
   return (
