7b39293b5117e1d542d7d1c3ed2c1ae2a121a46c
medazizktata <aziiizktata7@gmail.com>
2024-04-05 05:09:12 +0200

    Navbar refactoring (moved to new location)

--- Arena-gym-website/src/pages/Home/HeroSection.jsx
+++ Arena-gym-website/src/pages/Home/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/Navbar/Navbar";
 import BackgroundText from "../../components/BackgroundText";
 import BigCaroussel from "../../components/BigCaroussel";
 import ButtonYellow from "../../components/Buttons/ButtonYellow";
--- Arena-gym-website/src/pages/Home/Home.jsx
+++ Arena-gym-website/src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/Navbar/Navbar";
 import HeroSection from "./HeroSection";
 import ActivitiesSection from "./ActivitiesSection";
 import CoachesSection from "./CoachesSection";
 import About from "../About/About";
 import SearchBox from "../../components/SearchBox";
-import HamburgerCrossIcon from "../../components/HamburgerCrossIcon/HamburgerCrossIcon";
+import HamburgerCrossIcon from "../../components/Navbar/HamburgerCrossIcon/HamburgerCrossIcon";
 
 function Home() {
   return (
