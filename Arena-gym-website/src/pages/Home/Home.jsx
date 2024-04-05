7eedc1af2db61c1d9b72bc2c6cc300492c35fc18
medazizktata <aziiizktata7@gmail.com>
2024-04-05 04:52:05 +0200

    Hamburger to Cross Icon transition for mobile device Navbar

--- Arena-gym-website/src/pages/Home/Home.jsx
+++ Arena-gym-website/src/pages/Home/Home.jsx
@@ -5,15 +5,16 @@ import ActivitiesSection from "./ActivitiesSection";
 import CoachesSection from "./CoachesSection";
 import About from "../About/About";
 import SearchBox from "../../components/SearchBox";
+import HamburgerCrossIcon from "../../components/HamburgerCrossIcon/HamburgerCrossIcon";
 
 function Home() {
   return (
     <>
       <Navbar />
       <HeroSection />
-      <ActivitiesSection/>
+      {/* <ActivitiesSection/>
       <CoachesSection/> 
-      <SearchBox/>
+      <SearchBox/> */}
    
     </>
   );
