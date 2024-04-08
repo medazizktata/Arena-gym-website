f42925b5c33019b78c4148943448aeb1f528bda5
medazizktata <aziiizktata7@gmail.com>
2024-04-08 03:18:52 +0200

    Page intro comp responsivness fix

--- Arena-gym-website/src/components/PageIntroduction.jsx
+++ Arena-gym-website/src/components/PageIntroduction.jsx
@@ -7,10 +7,10 @@ function PageIntroduction({
   BGText = "",
   title,
   titleProps = "text-white text-center",
-  subTitle="",
+  subTitle = "",
   subTitleProps = "text-white text-center",
 }) {
-  const translateValue = 30 - BGText.length * 0.5;
+  const translateValue = 30 - BGText.length * 0.4;
 
   // Calculate extraHeight based on whether BGText is empty or not
   let extraHeight = 110;
@@ -24,7 +24,7 @@ function PageIntroduction({
         <div className="inline-block flex flex-col relative z-10">
           <div className="transform translate-y-28">
             <div
-              className={`w-[80%] mx-auto transform translate-y-${translateValue} opacity-80`}
+              className={`w-[80%] mx-auto transform translate-y-${translateValue} opacity-80 sm:translate-y-0 sm:scale-80 `}
             >
               <BackgroundText
                 text={`${BGText}`}
@@ -35,11 +35,13 @@ function PageIntroduction({
             </div>
           </div>
           <h1
-            className={`inline font-Gentona font-black text-9xl inline ${titleProps}`}
+            className={`inline font-Gentona font-black text-6xl md:text-8xl lg:text-9xl ${titleProps}`}
           >
             {title}
           </h1>
-          <h2 className={`inline ${subTitleProps}`}>{subTitle}</h2>
+          <h2 className={`inline text-lg sm:text-xl ${subTitleProps}`}>
+            {subTitle}
+          </h2>
         </div>
 
         <img
@@ -48,7 +50,7 @@ function PageIntroduction({
           alt="Background"
         />
         <div
-          className={`absolute inset-0 w-full h-[${extraHeight}%]`}
+          className={`absolute inset-0 w-full h-[${extraHeight+10}%]`}
           style={{
             background: `linear-gradient(to top, ${getColor(
               "blackBG"
