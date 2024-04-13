162495ea7da04581c0e064c0e35d47c4857a9cca
medazizktata <aziiizktata7@gmail.com>
2024-04-13 21:02:02 +0200

    Inscription page and comps small fixes

--- Arena-gym-website/src/pages/inscription/form_inscription.jsx
+++ Arena-gym-website/src/pages/inscription/form_inscription.jsx
@@ -1,85 +1,129 @@
+import React, { useState } from "react";
 
-import React, { useState } from 'react';
+import { useForm, FormProvider } from "react-hook-form";
 
-import { useForm, FormProvider } from 'react-hook-form';
+import InputField from "../../components/input";
+import DropdownField from "../../components/input_2";
+import DropdownField2 from "../../components/Input_2_disabled";
+import { IoMdMale } from "react-icons/io";
+import { IoMdFemale } from "react-icons/io";
+import ButtonYellow from "../../components/Buttons/ButtonYellow";
 
-import InputField from '../../components/input';
-import DropdownField from '../../components/input_2';
-import DropdownField2 from '../../components/Input_2_disabled';
 const Form_Ins = () => {
-
   const methods = useForm();
   const { handleSubmit, register, reset } = methods;
 
-  const [selectedGender, setSelectedGender] = useState('');
+  const [selectedGender, setSelectedGender] = useState("");
 
   const onSubmit = (data) => {
     // Ajout du genre sélectionné aux données du formulaire avant de les afficher
     const fullData = { ...data, gender: selectedGender };
     console.log(fullData); // Affiche les données du formulaire + genre dans la console
     reset(); // Réinitialise le formulaire et l'état local du genre après la soumission
-    setSelectedGender(''); // Réinitialise le genre
+    setSelectedGender(""); // Réinitialise le genre
   };
 
-
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center ">
       <FormProvider {...methods}>
-      <form onSubmit={handleSubmit(onSubmit)} className="bg-blackBG bg-opacity-70 border-2 border-white p-14  w-full max-w-2xl">
-        <h1 className="text-yellowMain text-4xl font-bold mb-6 text-center">REJOIGNEZ-NOUS</h1>
-        <p className="text-yellowMain text-sm mb-8 text-center">Veuillez remplir le formulaire pour vous inscrire et poursuivre vers la page de paiement</p>
-
-        <div className="flex gap-2 mb-4   ">
-          <div className='grow'>
-          <InputField name="nom" label="Nom" placeholder="Ex. Flen" type="text" register={register} />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="shadow-cardWhite border-2 border-white hover:shadow-cardYellowMain hover:border-yellowMain cursor-pointer transition all duration 500 font-robotoCon bg-blackBG bg-opacity-70 p-14 w-full max-w-2xl"
+        >
+          <h1 className="text-yellowMain text-7xl font-bold mb-6 text-center">
+            REJOIGNEZ-NOUS
+          </h1>
+          <p className="text-white text-base mb-8 text-center">
+            Veuillez remplir le formulaire pour vous inscrire et poursuivre vers
+            la page de paiement
+          </p>
+          <div className="flex flex-col gap-7">
+            <div className="flex gap-2">
+              <div className="grow">
+                <InputField
+                  name="nom"
+                  label="Nom"
+                  placeholder="Ex. Flen"
+                  type="text"
+                  register={register}
+                />
+              </div>
+              <div className="grow">
+                <InputField
+                  name="prenom"
+                  label="Prénom"
+                  placeholder="Ex. Flen"
+                  type="text"
+                  register={register}
+                />
+              </div>
+            </div>
+            <div className="">
+              <InputField
+                name="email"
+                label="E-mail"
+                placeholder="Ex. flen.falten@gmail.com"
+                type="email"
+                register={register}
+              />
+            </div>
+            <div className="">
+              <InputField
+                name="numero"
+                label="Numéro de téléphone"
+                placeholder="+216"
+                type="tel"
+                register={register}
+              />
+            </div>
+            <div className="">
+              <DropdownField2
+                name="pack"
+                label="Pack choisi"
+                options={["Pack 2 (560TND par 6 mois)"]}
+                register={register}
+              />
+            </div>
+            <div className="">
+              <DropdownField
+                name="plagesHoraires"
+                label="Plages horaires préférées pendant la semaine/week-end ?"
+                options={["Matin", "Après-midi", "Soir"]}
+                register={register}
+              />
+            </div>
           </div>
-          <div className='grow'>
-          <InputField name="prenom" label="Prénom" placeholder="Ex. Flen" type="text" register={register} />
-          </div>
-        </div>
-        <div className=" mb-4">
-
-        <InputField name="email" label="E-mail" placeholder="Ex. flen.falten@gmail.com" type="email" register={register} />
-        </div>
-        <div className=" mb-4">
-
-        <InputField name="numero" label="Numéro de téléphone" placeholder="+216" type="tel" register={register} />
-        </div>
-        <div className=" mb-4">
-
-        <DropdownField2 name="pack" label="Pack choisi" options={['Pack 2 (560TND par 6 mois)']}  register={register}  />
-        </div>
-        <div className=" mb-4">
-
-        <DropdownField name="plagesHoraires" label="Plages horaires préférées pendant la semaine/week-end ?" options={['Matin', 'Après-midi', 'Soir']} register={register} />
-        </div>
-        <div className="flex justify-around my-7 mt-8 mb-8">
-        <button
+          <div className="flex justify-around my-7 mt-8 mb-8">
+            <button
               type="button"
-              className={`p-1 w-24 h-24 shadow-md ${selectedGender === 'male' ? 'bg-white' : 'bg-black'} shadow-blue-500/50`}
-              onClick={() => setSelectedGender('male')}
+              className={`bg-blackBG border border-1 border-white w-24 h-24 shadow-cardSmallBlueMaleDark hover:shadow-cardBlueMale transition all duration 500
+              ${
+                selectedGender === "male"
+                  ? "bg-white hover:border-black"
+                  : "bg-black hover:border-yellowMain"
+              }`}
+              onClick={() => setSelectedGender("male")}
             >
-              {/* Assurez-vous que les chemins des images sont corrects */}
-              <img src="src/assets/Images/inscription/icons8-male-100.png" alt="Male" />
+              <IoMdMale className="text-7xl mx-auto text-blueMale" />
             </button>
             <button
               type="button"
-              className={`p-2 w-24 h-24 shadow-md ${selectedGender === 'female' ? 'bg-white' : 'bg-black'} shadow-pink-500/50`}
-              onClick={() => setSelectedGender('female')}
+              className={`bg-blackBG border border-1 border-white w-24 h-24 shadow-cardSmallPinkFemaleDark hover:shadow-cardPinkFemale transition all duration 500
+               ${
+                 selectedGender === "female"
+                   ? "bg-white hover:border-black"
+                   : "bg-black hover:border-yellowMain"
+               } `}
+              onClick={() => setSelectedGender("female")}
             >
-              {/* Assurez-vous que les chemins des images sont corrects */}
-              <img src="src/assets/Images/inscription/002-femelle-1.png" alt="Female" />
+              <IoMdFemale className="text-7xl mx-auto text-pinkFemale" />
             </button>
-        </div>
-
-        <button type="submit" className="bg-yellowMain text-black py-3 px-6 rounded w-full font-bold text-xl">
-          S'inscrire maintenant
-        </button>
-      </form>
+          </div>
+          <ButtonYellow buttonFont={"font-robotoCon"} fontWeight={"mt-10 w-full font-black"} buttonType={"submit"}> S'inscrire maintenant</ButtonYellow>
+        </form>
       </FormProvider>
     </div>
   );
 };
 
 export default Form_Ins;
-
