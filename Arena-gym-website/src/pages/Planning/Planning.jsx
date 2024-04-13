7b6cf9444650d8e3bdb40c6a22bca5aae6389c69
medazizktata <aziiizktata7@gmail.com>
2024-04-13 08:24:15 +0200

    Planning page comp v0

---
+++ Arena-gym-website/src/pages/Planning/Planning.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { getColor } from "../../utils/getColor";
+import PageIntroduction from "../../components/PageIntroduction";
+import SearchBox from "../../components/SearchBox";
+import FilterButton from "../../components/Buttons/FilterButton";
+import ButtonBlack from "../../components/Buttons/ButtonBlack";
+import FilterDropDown from "../../components/FilterDropDown";
+import FilterDropDownUpgraded from "../../components/FilterDropDownUpgraded";
+import DynamicIcon from "../../components/DynamicIcon";
+
+const Planning = () => {
+  return (
+    <>
+      <PageIntroduction
+        imgSrc={
+          "../src/assets/Images/Caroussel/Hero-section/Training_ground.jpg"
+        }
+        title="Planning"
+        titleProps="my-5 text-greyLight text-center uppercase text-6xl sm:text-7xl md:text-8xl lg:text-9xl"
+      />
+      <div className="flex justify-center">
+        <div className="">
+          {/*           <SearchBox width={600} />
+           */}{" "}
+        </div>
+        {/*     <FilterDropDown>Cours</FilterDropDown>
+         */}{" "}
+        <FilterDropDownUpgraded>Coaches</FilterDropDownUpgraded>
+     
+      </div>
+    </>
+  );
+};
+export default Planning;
