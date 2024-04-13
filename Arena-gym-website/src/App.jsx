fd28093f69d860003be36c4222a2672384f27c1d
medazizktata <aziiizktata7@gmail.com>
2024-04-13 08:14:37 +0200

    Added 2nd version of Gallery comp to App.jsx

--- Arena-gym-website/src/App.jsx
+++ Arena-gym-website/src/App.jsx
@@ -8,49 +8,68 @@ import ErrorPage from "./utils/ErrorPage";
 import Galerie from "./pages/Gallery/Galerie";
 import About from "./pages/About/About";
 import BelowFooter from "./components/BelowFooter.jsx";
+import GalerieV2 from "./pages/Gallery/Galeriev2.jsx";
+import Planning from "./pages/Planning/Planning.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} errorElement={<ErrorPage />} />
-        <Route
-          path="/inscription"
-          element={<Inscription />}
-          errorElement={<ErrorPage />}
-        />
-        <Route
-          path="/galerie"
-          element={<Galerie />}
-          errorElement={<ErrorPage />}
-        />
-        <Route
-          path="/activites"
-          element={<Home />}
-          errorElement={<ErrorPage />}
-        />
-        <Route
-          path="/coaches"
-          element={<Home />}
-          errorElement={<ErrorPage />}
-        />
-        <Route path="/blog" element={<Home />} errorElement={<ErrorPage />} />
-        <Route path="/tarifs" element={<Home />} errorElement={<ErrorPage />} />
-        <Route
-          path="/planning"
-          element={<Home />}
-          errorElement={<ErrorPage />}
-        />
-        <Route
-          path="/contact"
-          element={<Home />}
-          errorElement={<ErrorPage />}
-        />
-        <Route path="/about" element={<About />} errorElement={<ErrorPage />} />
-        <Route element={<ErrorPage />} />
-      </Routes>
-      <BelowFooter/>
+      <div className="relative">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Home />}
+            errorElement={<ErrorPage />}
+          />
+
+          <Route
+            path="/inscription"
+            element={<Inscription />}
+            errorElement={<ErrorPage />}
+          />
+          <Route
+            path="/galerie"
+            element={<GalerieV2 />}
+            errorElement={<ErrorPage />}
+          />
+          <Route
+            path="/activites"
+            element={<Home />}
+            errorElement={<ErrorPage />}
+          />
+          <Route
+            path="/coaches"
+            element={<Home />}
+            errorElement={<ErrorPage />}
+          />
+          <Route path="/blog" element={<Home />} errorElement={<ErrorPage />} />
+          <Route
+            path="/tarifs"
+            element={<Home />}
+            errorElement={<ErrorPage />}
+          />
+          <Route
+            path="/planning"
+            element={<Planning />}
+            errorElement={<ErrorPage />}
+          />
+          <Route
+            path="/contact"
+            element={<Home />}
+            errorElement={<ErrorPage />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+            errorElement={<ErrorPage />}
+          />
+        </Routes>
+      </div>
+      {/* <div className="mt-20 w-full bottom-0 mx-auto">
+        <BelowFooter />
+      </div> */}
     </>
   );
 }
