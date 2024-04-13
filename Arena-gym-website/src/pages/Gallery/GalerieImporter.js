f424e516608e0a29bcf0cedc1e4ba1d052755462
medazizktata <aziiizktata7@gmail.com>
2024-04-13 08:14:03 +0200

    GalerieImporter JS function to import from a folder

---
+++ Arena-gym-website/src/pages/Gallery/GalerieImporter.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const GalerieImporter = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const loadImages = async () => {
+      const imageModules = import.meta.glob(
+        "/src/assets/Images/Gallerie/*.{jpg,png,webp}"
+      );
+      const imagePaths = Object.keys(imageModules);
+
+      const loadedImages = await Promise.all(
+        imagePaths.map(async (path) => {
+          const module = await imageModules[path]();
+          const img = new Image();
+          img.src = module.default;
+          await img.decode(); // Ensure the image is fully loaded before extracting dimensions
+          return { src: module.default, width: img.width, height: img.height };
+        })
+      );
+
+      setImages(loadedImages);
+    };
+
+    loadImages();
+  }, []);
+
+  return images;
+};
+
+export default GalerieImporter;
