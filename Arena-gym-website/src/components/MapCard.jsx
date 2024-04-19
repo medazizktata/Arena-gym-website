0377ef7652fb73df6bd4cfea860f9c9a2c4bf68b
medazizktata <aziiizktata7@gmail.com>
2024-04-20 01:12:47 +0200

    Fixed text color Map Card comp

--- Arena-gym-website/src/components/MapCard.jsx
+++ Arena-gym-website/src/components/MapCard.jsx
@@ -5,7 +5,7 @@ const MapCard = ({ title, subtitle, url, width="1000px", height="500px" }) => {
     <div className="flex flex-col items-center justify-content min-h-screen px-4 font-urbanist">
       <div className="text-center my-8">
         <h1 className="font-bold text-white py-7 text-5xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">{title}</h1>
-        <p className="mx-auto text-gray-300 text-xl sm:text-3xl md:text-4xl lg:text-5xl lg:w-[80%] text-center">{subtitle}</p>
+        <p className="mx-auto text-white text-xl sm:text-3xl md:text-4xl lg:text-5xl lg:w-[80%] text-center">{subtitle}</p>
       </div>
       <div className="relative p-3 w-full max-w-4xl mx-auto">
         <iframe
