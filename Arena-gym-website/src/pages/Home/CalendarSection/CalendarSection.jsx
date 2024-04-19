31b4e027ce24a7a72bef292e305627eb5b694a24
medazizktata <aziiizktata7@gmail.com>
2024-04-20 01:17:19 +0200

    Calendar section & comp

---
+++ Arena-gym-website/src/pages/Home/CalendarSection/CalendarSection.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import BackgroundText from "../../../components/BackgroundText";
+import ButtonYellow from "../../../components/Buttons/ButtonYellow";
+import Example from "./Calendar";
+
+function CalendarSection() {
+  return (
+    <>
+      <div className="my-10 md:mx-20  px-14 relative flex flex-col items-center ">
+        <div className="absolute z-1 w-full top-10 opacity-50">
+          <BackgroundText
+            className=""
+            text={"PLANNING"}
+            fontFamily={"urbanist"}
+            strokeColor={"white"}
+            textSize={20}
+          />
+        </div>
+        <h1 className="font-urbanist text-center font-bold text-white text-4xl md:text-5xl lg:text-6xl xl:text-7xl relative z-10 my-4">
+          Consultez notre planning du mois
+        </h1>
+        <h3 className="text-white text-2xl lg:text-4xl text-urbanist text-center w-[80%] mx-auto">
+          Découvrir davantage les plans d’entrainement offerts adaptés à vos
+          attentes et besoins
+        </h3>
+      </div>
+
+      <div className="flex flex-col gap-20 justify-center items-center text-lg relative z-10">
+        <span>example</span>
+        <Example />
+
+        <ButtonYellow
+          buttonFont="font-robotoCon"
+          fontWeight="font-black hover:scale-105"
+          paddingX={10}
+        >
+          Voir nos Plans
+        </ButtonYellow>
+      </div>
+    </>
+  );
+}
+
+export default CalendarSection;
