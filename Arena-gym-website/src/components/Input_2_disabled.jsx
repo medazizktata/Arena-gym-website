253cd6f6737488d2712793a4da92d35b3c5f88ce
medazizktata <aziiizktata7@gmail.com>
2024-04-20 01:12:31 +0200

    Added focus CSS feature for inputs

--- Arena-gym-website/src/components/Input_2_disabled.jsx
+++ Arena-gym-website/src/components/Input_2_disabled.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
 
-const DropdownField2 = ({ name, label, options , type}) => {
+const DropdownField2 = ({ name, label, options }) => {
   const { register } = useFormContext();
 
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col">
       <label className="text-white mb-2">{label}</label>
-      <div className=" border-b-4 border-t-2 border-l-2 border-r-2 border-white">
+      <div className="border-b-4 border-t-2 border-l-2 border-r-2 border-white transition duration-300">
         <select
-        {...register(name)}
-          className="bg-blackBG text-gray-500 w-full py-2 px-3 cursor-pointer appearance-none"
-          
+          {...register(name)}
+          className="bg-blackBG text-gray-500 w-full py-2 px-3 cursor-pointer appearance-none focus:outline-none"
+          onFocus={(e) => {
+            e.target.parentNode.classList.add('border-yellowMain');
+          }}
+          onBlur={(e) => {
+            e.target.parentNode.classList.remove('border-yellowMain');
+          }}
         >
           {options.map((option, index) => (
             <option key={index} value={option} className="bg-blackBG text-white">
--- Arena-gym-website/src/components/input.jsx
+++ Arena-gym-website/src/components/input.jsx
@@ -1,40 +1,24 @@
-/*
-
-import React from 'react';
-
-const InputField = ({ label, placeholder , type, value }) => {
-    return (
-        <div className="flex flex-col ">
-          <label className="text-white mb-2">{label}</label>
-          <div className=' border-b-4 border-t-2 border-l-2 border-r-2 border-white'>
-          <input
-            type={type}
-            value={value}
-            placeholder={placeholder}
-            className="bg-transparent w-full text-white placeholder-gray-500 py-2 px-3"
-          />
-          </div>
-        </div>
-      );
-    };
-
-export default InputField;
-*/
- 
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
 
 const InputField = ({ name, label, placeholder, type }) => {
-  const { register } = useFormContext(); // Utiliser useFormContext pour accéder à register
+  const { register } = useFormContext();
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col">
       <label className="text-white mb-2">{label}</label>
-      <div className='border-b-4 border-t-2 border-l-2 border-r-2 border-white'>
+      <div className="border-b-4 border-t-2 border-l-2 border-r-2 border-white transition duration-300">
         <input
-          {...register(name)} // Appliquer register avec le nom du champ
+          {...register(name)}
           type={type}
           placeholder={placeholder}
-          className="bg-transparent w-full text-white placeholder-gray-500 py-2 px-3"
+          className="bg-transparent w-full text-white placeholder-gray-500 py-2 px-3 focus:outline-none focus:ring-1 focus:ring-yellowMain"
+          onFocus={(e) => {
+            e.target.parentNode.classList.add('border-yellowMain');
+          }}
+          onBlur={(e) => {
+            e.target.parentNode.classList.remove('border-yellowMain');
+          }}
+          autoComplete='off'
         />
       </div>
     </div>
