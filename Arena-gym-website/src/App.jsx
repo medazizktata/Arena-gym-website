d7a2ea4358f08406d355069b9394af83de31f753
RaniaBenAbdallah-a <raniabenabdallah35@gmail.com>
2024-04-20 19:22:36 +0100

    App.jsx

--- Arena-gym-website/src/App.jsx
+++ Arena-gym-website/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import Home from './pages/Home/Home.jsx';
+import SearchBar from './components/SearchBar.jsx';
+import Popup from './components/Popup.jsx';
+import HeadEntraineurs from './components/HeadEntraineurs.jsx';
+import Navbar from './components/Navbar.jsx';
+import ImageHover from './components/ImageHover.jsx';
+import ContentEntraineurs from './components/ContentEntraineurs.jsx';
+import HeadApplication from './components/HeadApplication.jsx';
+import AppCode from './pages/Application/AppCode.jsx';
+import ContentApp from './components/ContentApp.jsx';
+import { PageApplication } from './pages/Application/PageApplication.jsx';
+
+
+
+
 
 function App() {
   return (
-    <Home/>
+    
+    <PageApplication/>
+   
   );
 }
 
