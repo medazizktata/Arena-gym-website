3bdf98babf91e3d9145f831882b4a366f6b5652b
medazizktata <aziiizktata7@gmail.com>
2024-04-22 01:22:48 +0200

    Pages entraineurs Rania V0

--- Arena-gym-website/src/components/HeadEntraineurs.jsx
+++ Arena-gym-website/src/components/HeadEntraineurs.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "./Navbar";
 import ImageHover from "./ImageHover";
 import SearchBar from "./SearchBar"
 
@@ -8,15 +7,10 @@ import SearchBar from "./SearchBar"
 function HeadEntraineurs() {
     return (
      <div className="relative ">
-         <h1 className="font-Gentona absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center font-bold text-7xl  ">
+         <h1 className="font-Gentona absolute top-1/2 left-1/2 transform -translate-x-1/2 text-white text-center font-bold text-7xl  ">
             NOS ENTRAINEURS</h1>
-         <img src=".\src\assets\Images\Entraineurs.png" className=' w-full h-25 ' />
+         <img src=".\src\assets\Images\Entraineurs.png" className=' w-full h-25' />
      </div>
-    
-             
-
-        
-      
     )
 
 }
---
+++ Arena-gym-website/src/pages/Coaches/PageEntraineurs.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import HeadEntraineurs from "../../components/HeadEntraineurs";
+import SearchBar from "../../components/SearchBar";
+import ContentEntraineurs from "../../components/ContentEntraineurs";
+import SearchBox from "../../components/SearchBox";
+
+export function PageEntraineurs() {
+  return (
+    <>
+      <HeadEntraineurs />
+      <div className="flex justify-center items-center transform scale-150 my-10">
+        <SearchBox />
+      </div>
+      <ContentEntraineurs />
+    </>
+  );
+}
