9cd77d60d26682fd50060914480e6010c13819cc
medazizktata <aziiizktata7@gmail.com>
2024-04-22 12:47:02 +0200

    Activities page ready to ship

--- Arena-gym-website/src/pages/activités/activité1.jsx
+++ Arena-gym-website/src/pages/activités/activité1.jsx
@@ -1,123 +1,108 @@
-import SubscriptionBox from "../../components/FichePrix";
-import Form_Ins from "../../components/form_inscription";
-import Navbar from "../../components/Navbar/Navbar";
-import { getColor } from "../../utils/getColor";
-import CustomizableTextBox from "../../components/CustomizableTextBox";
-import ButtonBlack from "../../components/Buttons/ButtonBlack";
+import React, { useState } from "react";
+import PageIntroduction from "../../components/PageIntroduction";
 import ActivityCard from "../../components/ActivityCard";
-import Pagination from "../../components/Pagination";
 import BackgroundText from "../../components/BackgroundText";
-import DropdownField from "../../components/input_2";
-import BelowFooter from "../../components/BelowFooter";
+import DropdownFieldVierge from "../../components/DropDownFieldVierge";
 
-
+import Pagination from "../../components/Pagination";
 
 function Activite() {
   const label = "Objectifs à atteindre";
-  const options = ["Choose Option", "Weight Loss", "Body Building", "Cross Fit", "Dance" ];
+  const options = [
+    "Choisir Objectif",
+    "Perte de poids",
+    "Musculation",
+    "Cross Fit",
+    "Danse",
+  ];
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const activitiesPerPage = 8;
+
+  const [activities, setActivities] = useState([
+    { name: "Strength Training", img: "/images/gym-workout-plan-for-gaining-muscle_header.jpg" },
+    { name: "Spinning", img: "/images/1000_F_524378456_7iH4roEgy8t8351zKypjRumlIbRfwhIO.jpg" },
+    { name: "HIIT Workout", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Pilates", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Zumba", img: "/images/gym-workout-plan-for-gaining-muscle_header.jpg" },
+    { name: "Cycling", img: "/images/1000_F_524378456_7iH4roEgy8t8351zKypjRumlIbRfwhIO.jpg" },
+    { name: "Tabata", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Aerobics", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Boxing", img: "/images/gym-workout-plan-for-gaining-muscle_header.jpg" },
+    { name: "Plyometrics", img: "/images/1000_F_524378456_7iH4roEgy8t8351zKypjRumlIbRfwhIO.jpg" },
+    { name: "Swimming", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Barre Workout", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Power Yoga", img: "/images/gym-workout-plan-for-gaining-muscle_header.jpg" },
+    { name: "Rowing", img: "/images/1000_F_524378456_7iH4roEgy8t8351zKypjRumlIbRfwhIO.jpg" },
+    { name: "Circuit Training", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Kettlebell Workout", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Tai Chi", img: "/images/gym-workout-plan-for-gaining-muscle_header.jpg" },
+    { name: "Bootcamp", img: "/images/1000_F_524378456_7iH4roEgy8t8351zKypjRumlIbRfwhIO.jpg" },
+    { name: "Stretching", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Kickboxing", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "TRX Training", img: "/images/gym-workout-plan-for-gaining-muscle_header.jpg" },
+    { name: "Crossfit", img: "/images/1000_F_524378456_7iH4roEgy8t8351zKypjRumlIbRfwhIO.jpg" },
+    { name: "Bodyweight Exercises", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Calisthenics", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Functional Training", img: "/images/gym-workout-plan-for-gaining-muscle_header.jpg" },
+    { name: "Prenatal Yoga", img: "/images/1000_F_524378456_7iH4roEgy8t8351zKypjRumlIbRfwhIO.jpg" },
+    { name: "Postnatal Workout", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Meditation", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "High-Intensity Interval Training (HIIT)", img: "/images/gym-workout-plan-for-gaining-muscle_header.jpg" },
+    { name: "Step Aerobics", img: "/images/1000_F_524378456_7iH4roEgy8t8351zKypjRumlIbRfwhIO.jpg" },
+    { name: "Piloxing", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Water Aerobics", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Gymnastics", img: "/images/gym-workout-plan-for-gaining-muscle_header.jpg" },
+    { name: "Kickboxing Fitness", img: "/images/1000_F_524378456_7iH4roEgy8t8351zKypjRumlIbRfwhIO.jpg" },
+    { name: "Tabata Training", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Senior Fitness", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Barre Pilates", img: "/images/gym-workout-plan-for-gaining-muscle_header.jpg" },
+    { name: "Interval Training", img: "/images/1000_F_524378456_7iH4roEgy8t8351zKypjRumlIbRfwhIO.jpg" },
+    { name: "Parkour", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+    { name: "Aerial Yoga", img: "/images/dumbbells-gym-64a3f4bc2fe31.jpg" },
+  ]);
+  
+
+  const indexOfLastActivity = currentPage * activitiesPerPage;
+  const indexOfFirstActivity = indexOfLastActivity - activitiesPerPage;
+  const currentActivities = activities.slice(indexOfFirstActivity, indexOfLastActivity);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div>
-      
-      <div className="relative min-h-screen flex flex-col items-start">
-        <div className="absolute inset-0 h-full z-10 w-auto">
-          <img
-            src="src/assets/Images/Caroussel/Hero-section/Training_ground.jpg"
-            className="absolute inset-0 w-full h-full object-cover"
-            alt="Background"
-          />
-          <div
-            className="absolute inset-0 w-full"
-            style={{
-              background: `linear-gradient(to top, ${getColor(
-                "blackBG"
-              )}, rgba(255,255,255,0))`,
-            }}
-          ></div>
-        </div>
-        <div className="z-10 ml-10 m-40">
-          <div className="mb-1">
-            <h1 className="text-greyLight font-Gentona font-black lg:text-9xl md:text-7xl sm:text-5xl text-5xl" style={{ opacity: 0.7, marginTop: "300px" }}>
-              Nos Activités
-            </h1>
-          </div>
-          </div>
-          </div>
-          
-      <div className='w-3/4 m-auto' >
-        <DropdownField label={label} options={options}  />
+      <PageIntroduction
+        imgSrc="../src/assets/Images/Caroussel/Hero-section/Training_ground.jpg"
+        title="Nos Activités"
+        titleProps="text-greyLight ml-[15%] uppercase text-6xl sm:text-7xl md:text-8xl lg:text-9xl"
+      />
+      <div className="my-20 -mt-8 w-3/4 m-auto">
+        <DropdownFieldVierge
+          name="plagesHoraires"
+          label={label}
+          options={options}
+        />
       </div>
-
-      {/* Render cards above background text */}
-      <div className="mb-10 relative z-15 " >
-
-      <div className="absolute z-1 w-full top-1/2 transform -translate-y-1/2 opacity-50 flex flex-col justify-between">
-          <BackgroundText
-            className=""
-            text={"Activite"}
-            fontFamily={"urbanist"}
-            strokeColor={"white"}
-            textSize={20}
-          />
-          <BackgroundText
-            className=""
-            text={"Activite"}
-            fontFamily={"urbanist"}
-            strokeColor={"white"}
-            textSize={20}
-            style={{marginTop:'100px'}}
-          />
-          
+      <div className="mx-auto">
+      <div className="flex flex-col justify-center items-center m-auto mx-20">
+        <div className="w-full scale-100 mx-20 grid justify-items-center items-center grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10 max-sm:min-w-[90%] max-sm:max-w-screen-[500px] max-sm:mx-10 max-sm:scale-90  ">
+          {currentActivities.map((activity, index) => (
+            <ActivityCard key={index} img={activity.img} nom={activity.name} />
+          ))}
         </div>
-        <div className="flex flex-col gap-1 justify-center items-center z-10 scale-90 md:flex-row" style={{ marginLeft:'10%', marginRight:'10%', display: 'flex', justifyContent: 'space-between' }}>
-            <ActivityCard img='images/gym-workout-plan-for-gaining-muscle_header.jpg' nom='Body Building' icon='images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png' />
-            <ActivityCard img='images/1000_F_524378456_7iH4roEgy8t8351zKypjRumlIbRfwhIO.jpg' nom='RPM' icon='images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png' />
-            <ActivityCard img='images/dumbbells-gym-64a3f4bc2fe31.jpg' nom='Fitness' icon='images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png' />
-        </div>
-
-
-        {/* Second set of cards */}
-        <div className=" flex flex-col gap-1 justify-center items-center z-10 scale-90 md:flex-row" style={{display: 'flex', justifyContent: 'space-between', marginLeft:'10%', marginRight:'10%' }}>
-            <ActivityCard img='images/gym-workout-plan-for-gaining-muscle_header.jpg' nom='Body Building' icon='images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png' className=" md:w-1/3 lg:w-1/3 xl:w-1/5" />
-            <ActivityCard img='images/1000_F_524378456_7iH4roEgy8t8351zKypjRumlIbRfwhIO.jpg' nom='RPM' icon='images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png' />
-            <ActivityCard img='images/dumbbells-gym-64a3f4bc2fe31.jpg' nom='Fitness' icon='images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png' />
-        </div>
-
-
-        {/* Third set of cards */}
-        <div className=" flex flex-col gap-1 justify-center items-center z-10 scale-90 md:flex-row" style={{display: 'flex', justifyContent: 'space-between', marginLeft:'10%', marginRight:'10%' }}>
-          <ActivityCard img='images/gym-workout-plan-for-gaining-muscle_header.jpg' nom='stoufa' icon='images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png' />
-          <ActivityCard img='images/gym-workout-plan-for-gaining-muscle_header.jpg' nom='stoufa' icon='images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png' />
-          <ActivityCard img='images/gym-workout-plan-for-gaining-muscle_header.jpg' nom='stoufa' icon='images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png' />
-        </div>
-
       </div>
-
-      {/* Pagination */}
-      <Pagination className=" mx-auto" />
+      </div>
+      <div className="my-10">
+        <Pagination
+          currentPage={currentPage}
+          totalPages={Math.ceil(activities.length / activitiesPerPage)}
+          onPageChange={handlePageChange}
+        />
+      </div>
     </div>
   );
 }
 
-const data = [
-  {
-    name: 'RPM',
-    img: 'images/gym-workout-plan-for-gaining-muscle_header.jpg',
-    icon:'images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png'
-  },
-  {
-    name: 'Cardio',
-    img: 'images/Gym-structure-1080x675.png',
-    icon:'images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png'
-  },
-  {
-    name: 'Musculation',
-    img: 'images/dumbbells-gym-64a3f4bc2fe31.jpg',
-    icon:'images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png'
-  },
-  {
-    name: 'boxe',
-    img: 'images/boxe-anglaise-debutant_dojo.jpg',
-    icon:'images/6dcc2d2ac756fe47587fd2be39913e47-removebg-preview.png'
-  }
-];
 export default Activite;
