18ba137d3a0af8b9cfb1811fa6140e4cafef6af6
medazizktata <aziiizktata7@gmail.com>
2024-04-25 16:42:44 +0200

    Changed icon

--- Arena-gym-website/src/components/ConsulterGallerie.jsx
+++ Arena-gym-website/src/components/ConsulterGallerie.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IoPlayCircleOutline } from "react-icons/io5";
+
 import { faImages } from '@fortawesome/free-solid-svg-icons';
 
 const ConsulterGalerieButton = ({className}) => {
     return (
         <div className={`flex items-center gap-3 h-16 text-white ${className}`}>
 
-            <FontAwesomeIcon icon={faImages} className="galerie-icon" />
+           {/*  <FontAwesomeIcon icon={faImages} className="galerie-icon" /> */}
+            <IoPlayCircleOutline className="" icon={faImages}/>
+
             <button className="galerie-button font-robotoCon"> Consulter notre galerie</button>
         </div>
     );
