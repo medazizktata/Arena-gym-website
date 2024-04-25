6c8f8714641b5dab7e6fb00c81df9e1892b73cfa
medazizktata <aziiizktata7@gmail.com>
2024-04-25 16:43:09 +0200

    CSS fix

--- Arena-gym-website/src/components/Navbar/Navbar.jsx
+++ Arena-gym-website/src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ function Navbar() {
     <nav className="">
       <div className="fixed top-0 left-0 w-full z-50">
         <div
-          className={`flex justify-between px-10 py-5 mx-auto text-white ${
+          className={`flex justify-between px-10 py-5 text-white ${
             navbarBackground ? "bg-blackBG" : "bg-blackBG"
           } transition-all duration-900`}
         >
@@ -130,7 +130,7 @@ function Navbar() {
               <ButtonYellow
                 buttonFont="font-robotoCon"
                 fontWeight="font-black"
-                paddingX={8}
+                paddingX={10}
               >
                 Join Us
               </ButtonYellow>
@@ -140,7 +140,7 @@ function Navbar() {
       </div>
       {/* Side Menu */}
       <div
-        className={`fixed top-0 right-0 h-full bg-blackBG z-40 lg:w-auto transition-all overflow-y-auto ${
+        className={`fixed top-10 right-0 h-full bg-blackBG z-40 lg:w-auto transition-all overflow-y-auto ${
           isMenuOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
