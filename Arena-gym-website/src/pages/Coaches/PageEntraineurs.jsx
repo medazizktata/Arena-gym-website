8667d3a5e7380dc6ad204b352b5421aecbf5bcf1
medazizktata <aziiizktata7@gmail.com>
2024-04-29 01:13:37 +0200

    Added page introduction comp

--- Arena-gym-website/src/pages/Coaches/PageEntraineurs.jsx
+++ Arena-gym-website/src/pages/Coaches/PageEntraineurs.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import HeadEntraineurs from "../../components/HeadEntraineurs";
 import ContentEntraineurs from "../../components/ContentEntraineurs";
 import SearchBox from "../../components/SearchBox";
+import PageIntroduction from "../../components/PageIntroduction";
 
 export function PageEntraineurs() {
   return (
     <>
-      <HeadEntraineurs />
+      <PageIntroduction
+        imgSrc={"src/assets/Images/Tarifs/Secret-8 1.png"}
+        title="Nos entraineurs"
+        titleProps="mb-10 max-sm:text-6xl max-md:text-7xl text-8xl text-white text-center"
+        subTitleProps="max-sm:text-4xl max-md:text-6xl md:text-6xl text-white text-center"
+      />
       <div className="flex justify-center items-center -mt-10 mb-10">
         <SearchBox width={600} />
       </div>
+
       <ContentEntraineurs />
     </>
   );
