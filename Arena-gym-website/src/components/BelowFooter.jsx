0b4fbc8d7f4d5ee57f20e58d9b7d8d63da5a07c9
medazizktata <aziiizktata7@gmail.com>
2024-04-29 15:21:45 +0200

    Responsive footer

--- Arena-gym-website/src/components/BelowFooter.jsx
+++ Arena-gym-website/src/components/BelowFooter.jsx
@@ -4,14 +4,14 @@ import { FaCopyright } from 'react-icons/fa';
 function BelowFooter() {
   return (
     <>
-      <div className="mt-5 border-t border-white-600 py-3 px-20 flex justify-between items-center text-white text-sm font-regular font-robotoCon text-center">
-        <div className="flex items-center justify-content">
-          <FaCopyright className="mr-2" />
+      <div className="mt-5 border-t border-white-600 py-3 flex justify-between items-center text-white text-sm font-robotoCon sm:px-20 max-sm:px-5 ">
+        <div className="flex items-center justify-between max-sm:justify-center">
+          <FaCopyright className="text-sm mr-2 max-sm:text-center" />
           <h3>2024 Arena Gym Website Made By ENIT Junior Entreprise</h3>
         </div>
         <div className="flex gap-8">
-          <a href="#" className="text-white">Terms of Use</a>
-          <a href="#" className="text-white">Privacy Policy</a>
+          <a href="#" className="text-white max-sm:hidden">Terms of Use</a>
+          <a href="#" className="text-white max-sm:hidden">Privacy Policy</a>
         </div>
       </div>
     </>
