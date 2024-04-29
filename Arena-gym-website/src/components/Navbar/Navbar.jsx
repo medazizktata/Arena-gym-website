5084d85a87f02cd4c6d71a12aa9d35bd82b16c04
medazizktata <aziiizktata7@gmail.com>
2024-04-29 15:21:31 +0200

    Glass effect on sidebar + scroll shift effect same as navbar + react-scroll feature on contact link (bg hover needs fixing)

--- Arena-gym-website/src/components/Navbar/Navbar.jsx
+++ Arena-gym-website/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import ImageHover from "../ImageHover";
 import ButtonYellow from "../Buttons/ButtonYellow";
 import HamburgerIcon from "./HamburgerCrossIcon/HamburgerCrossIcon";
 import { Link, useLocation } from "react-router-dom";
+import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -15,11 +16,16 @@ function Navbar() {
 
   const [navbarBackground, setNavbarBackground] = useState(false);
 
+  const [sidebarBackground, setSidebarBackground] = useState(false);
+
   useEffect(() => {
     const handleScroll = () => {
       const isScrolled = window.scrollY > 200;
       if (isScrolled !== navbarBackground) {
         setNavbarBackground(isScrolled);
+      }
+      if (isScrolled !== sidebarBackground) {
+        setSidebarBackground(isScrolled);
       }
     };
 
@@ -28,21 +34,19 @@ function Navbar() {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [navbarBackground]);
+  }, [navbarBackground, sidebarBackground]);
+
 
   return (
     <nav className="">
       <div className="fixed top-0 left-0 w-full z-50">
         <div
-          className={`flex justify-between px-10 py-5 text-white ${
-            navbarBackground ? "bg-blackBG" : "bg-blackBG"
+          className={`flex justify-between px-10 py-5 text-white bg-blackBG  ${
+            navbarBackground ? "bg-opacity-100" : "bg-opacity-0"
           } transition-all duration-900`}
         >
           <div className="inline-flex items-center text-center w-40 hover:text-yellowMain transition duration-500 cursor-pointer">
-            <Link
-              to="/"
-              className="group hover:text-yellowMain transition duration-300"
-            >
+            <Link to={"/"}>
               <ImageHover
                 originalSrc="src/assets/Logo/Final Logo/White/1x/Gym Premium White.png"
                 hoverSrc="src/assets/Logo/Final Logo/Yellow/FDEA1D/1x/Gym Premium Yellow FDEA1D.png"
@@ -55,12 +59,15 @@ function Navbar() {
           </div>
 
           {/* Desktop Menu Links */}
-          <div className="hidden lg:flex lg:space-x-3 items-center text-center font-urbanist font-semibold">
-            <NavLink to="/home" active={location.pathname === "/home"}>
-              <span className="block">Acceuil</span>
-              {/* <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-yellowMain"></span> */}
-              <span className="absolute inset-0 hover:bg-white opacity-15 transition duration-300 "></span>
-            </NavLink>
+          <div className="hidden lg:flex lg:space-x-2 md:space-x-2 items-center text-center font-urbanist font-semibold">
+            <ScrollLink to="home" spy={true} smooth={true} duration={500} className="">
+              <NavLink to="/home" active={location.pathname === "/home"}>
+                <span className="block">Acceuil</span>
+                {/* <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-yellowMain"></span> */}
+                <span className="absolute inset-0 hover:bg-white opacity-15 transition duration-300 "></span>
+
+              </NavLink>
+            </ScrollLink>
             <NavLink
               to="/activites"
               active={location.pathname === "/activites"}
@@ -115,15 +122,19 @@ function Navbar() {
               {/* <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-yellowMain"></span> */}
               <span className="absolute inset-0  hover:bg-white opacity-15 transition duration-300 "></span>
             </NavLink>
-            <NavLink
-              to="/contact"
-              active={location.pathname === "/contact"}
+            <ScrollLink
+              to="home#contact"
+              spy={true}
+              smooth={true}
+              duration={500}
               className="text-white p-2 group relative hover:text-yellowMain transition duration-300"
             >
-              <span className="block">Contact</span>
-              {/* <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-yellowMain"></span> */}
-              <span className="absolute inset-0  hover:bg-white opacity-15 transition duration-300 "></span>
-            </NavLink>
+              <Link to="/home#contact">
+                <span className="block">Contact</span>
+                <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-yellowMain"></span>
+                <span className="absolute inset-0 hover:bg-white opacity-15 transition duration-300 "></span>
+              </Link>
+            </ScrollLink>
             <NavLink
               to="/about"
               active={location.pathname === "/about"}
@@ -133,7 +144,7 @@ function Navbar() {
               {/* <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-yellowMain"></span> */}
               <span className="absolute inset-0  hover:bg-white opacity-15 transition duration-300 "></span>
             </NavLink>
-            <NavLink to="/tarifs" active={location.pathname === "/tarifs"}>
+            <Link to="/tarifs" active={location.pathname === "/tarifs"}>
               <ButtonYellow
                 buttonFont="font-robotoCon"
                 fontWeight="font-black"
@@ -141,15 +152,17 @@ function Navbar() {
               >
                 Join Us
               </ButtonYellow>
-            </NavLink>
+            </Link>
           </div>
         </div>
       </div>
       {/* Side Menu */}
       <div
-        className={`fixed top-10 right-0 h-full bg-blackBG z-40 lg:w-auto transition-all overflow-y-auto ${
+        className={`fixed pt-5 right-0 h-full z-40 lg:w-auto transition-all overflow-y-auto  ${
           isMenuOpen ? "translate-x-0" : "translate-x-full"
-        }`}
+        } ${
+          sidebarBackground ? "bg-blackBG bg-opacity-100 border-l-2 border-b-2 border-white" : "bg-opacity-90 backdrop-filter backdrop-blur-lg border-l-2 border-b-2 border-greyDark"
+        } transition-background-color duration-900`}
       >
         <div className="flex flex-col gap-5 text-2xl items-start pt-5 w-60 lg:w-64 space-x-3 font-urbanist font-semibold">
           <NavLink
@@ -169,6 +182,7 @@ function Navbar() {
             <span className="block">Acceuil</span>
             {/* <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-yellowMain"></span> */}
             <span className="absolute inset-0  hover:bg-white opacity-15 transition duration-300 "></span>
+            
           </NavLink>
           <NavLink
             to="/activites"
@@ -224,15 +238,19 @@ function Navbar() {
             {/* <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-yellowMain"></span> */}
             <span className="absolute inset-0  hover:bg-white opacity-15 transition duration-300 "></span>
           </NavLink>
-          <NavLink
-            to="/contact"
-            active={location.pathname === "/contact"}
-            className="text-white p-2 group relative hover:text-yellowMain transition duration-300"
-          >
-            <span className="block">Contact</span>
-            {/* <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-yellowMain"></span> */}
-            <span className="absolute inset-0  hover:bg-white opacity-15 transition duration-300 "></span>
-          </NavLink>
+          <ScrollLink
+              to="home#contact"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className="text-white p-2 group relative hover:text-yellowMain transition duration-300"
+            >
+              <Link to="/home#contact">
+                <span className="block">Contact</span>
+                <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-yellowMain"></span>
+                <span className="absolute inset-0 hover:bg-white opacity-15 transition duration-300 "></span>
+              </Link>
+            </ScrollLink>
           <NavLink
             to="/about"
             active={location.pathname === "/about"}
@@ -261,7 +279,9 @@ const NavLink = ({ to, active, children }) => {
           active ? "max-w-full " : "group-hover:max-w-full"
         }`}
       ></span>
-      <span className="absolute inset-0 hover:bg-white opacity-15 transition duration-300 "></span>
+      {!active && (
+        <span className="absolute inset-0 hover:bg-white opacity-15 transition duration-300 "></span>
+      )}
     </Link>
   );
 };
